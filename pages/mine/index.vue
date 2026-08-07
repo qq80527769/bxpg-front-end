6de0e56b852524f6d21d76ecdb6bb43e973b401f
网吧 <wb@wb.com>
2026-08-07 10:14:40 +0800

    个人中心修改

--- pages/mine/index.vue
+++ pages/mine/index.vue
@@ -1,168 +1,160 @@
 <template>
 	<view class="d_mine">
 
-		<view class="d_m_user d_flex">
-			<u-avatar class="fl" size="100rpx" :src="userInfo.avatar" shape="square"></u-avatar>
-			<view class="con d_flex">
-				<view>{{userInfo.username}}</view>
-				<view class="d_flex" @tap="copyData">
-					<text style="margin-right: 20rpx;">ID: {{userInfo.id}}</text>
-					<u--image :showLoading="true" src="../../static/images/copy.png" width="28rpx"
-						height="28rpx"></u--image>
-				</view>
-			</view>
-			<<!-- view class="fr border_shadow">
-				<text>按钮</text>
-		</view> -->
-	</view>
-	<view class="d_m_detail">
-		<u-grid :border="false" col="2">
-			<u-grid-item class="detail_grid_item">
-				<text>{{gameAmount.money}}</text>
-				<text>{{$t('mine.text1')}}</text>
-			</u-grid-item>
-			<u-grid-item class="detail_grid_item">
-				<view class="d_flex">
-					<text style="margin-right: 10rpx;">{{gameAmount.balance}}</text>
-					<u-icon name="reload" color="#999" size="34rpx" @tap="changeGameAmount"></u-icon>
-				</view>
-				<text>{{$t('mine.text22')}}</text>
-			</u-grid-item>
-		</u-grid>
-	</view>
-	<view class="d_m_vip d_bgColor border_shadow d_border12">
-		<view class="vi_top d_flex">
-			<view class="v_t_fl">
-				<u--image :showLoading="true" src="../../static/images/vip-background.png" width="200rpx"
-					height="160rpx" mode="scaleToFill">
-				</u--image>
-				<text>VIP {{userInfo.level}}</text>
-			</view>
-			<view class="v_t_fr">
-				<view style="margin-bottom: 10rpx;">
-					<text style="margin-right: 20rpx">{{$t('mine.text3')}}: </text>
-					<text style="color: #ff5a2c;">{{userInfo.recharge}}</text>
-				</view>
-				<view>
-					<u-line-progress :percentage="userInfo.recharge_b" activeColor="#37dfb1"></u-line-progress>
-				</view>
-				<view style="height: 24rpx;"></view>
-				<view style="margin-bottom: 10rpx;">
-					<text style="margin-right: 20rpx">{{$t('mine.text4')}}: </text>
-					<text style="color: #ff5a2c;">{{userInfo.bet}}</text>
-				</view>
-				<view>
-					<u-line-progress :percentage="userInfo.bet_b" activeColor="#37dfb1"></u-line-progress>
-				</view>
-			</view>
-		</view>
-		<view class="vi_grid">
-			<u-grid :border="false" class="grid" col="2">
-				<u-grid-item class="vi_grid_item d_border12 d_flex"
-					@tap="$tab.navigateTo('/pages/mine/account/index?index=0')">
-					<text style="margin-right: 20rpx;" class="grid-text">{{$t('mine.text5')}}</text>
-					<u-icon name="../../static/images/conta.png" size="38rpx"></u-icon>
+		<view class="d_m_detail">
+			<u-grid :border="false" col="2">
+				<u-grid-item class="detail_grid_item">
+					<view class="d_m_user d_flex">
+						<u-avatar class="fl" size="100rpx" :src="userInfo.avatar" shape="square"></u-avatar>
+						<view class="con d_flex">
+							<view>{{userInfo.username}}</view>
+							<view class="d_flex" @tap="copyData">
+								<text style="margin-right: 20rpx;">ID: {{userInfo.id}}</text>
+								<u--image :showLoading="true" src="../../static/images/copy.png" width="28rpx"
+								          height="28rpx"></u--image>
+							</view>
+						</view>
+					</view>
 				</u-grid-item>
-				<u-grid-item class="vi_grid_item d_border12 d_flex"
-					@tap="$tab.navigateTo('/pages/mine/account/index?index=1')">
-					<text style="margin-right: 20rpx;" class="grid-text">{{$t('mine.text6')}}</text>
-					<u-icon name="../../static/images/conta.png" size="38rpx"></u-icon>
+				<u-grid-item class="detail_grid_item" >
+					<text>{{gameAmount.money}}</text>
+					<text>{{$t('mine.text1')}}</text>
 				</u-grid-item>
 			</u-grid>
 		</view>
-	</view>
-	<view class="d_m_list border_shadow d_border12 d_bgColor">
-		<view style="margin-top: 20rpx; color: #46b5fb;">
-			{{$t('mine.text7')}}
-		</view>
-		<view class="li_items d_flex" @tap="$tab.navigateTo('/pages/mine/account/index?index=2')">
-			<view>{{$t('mine.text8')}}</view>
-			<u-icon name="arrow-right" color="#888" size="30rpx"></u-icon>
-		</view>
-		
-		<view class="li_items d_flex" @tap="$tab.navigateTo('/pages/mine/backwater/index?index=2')">
-			<view>{{$t('title.text26')}}</view>
-			<u-icon name="arrow-right" color="#888" size="30rpx"></u-icon>
-		</view>
-		
-		
-		<view class="li_items d_flex" @tap="showConversion=true">
-			<view>{{$t('mine.text9')}}</view>
-			<u-icon name="arrow-right" color="#888" size="30rpx"></u-icon>
-		</view>
-		<view class="li_items d_flex" @tap="$tab.navigateTo('/pages/mine/play-record/index')">
-			<view>{{$t('mine.text10')}}</view>
-			<u-icon name="arrow-right" color="#888" size="30rpx"></u-icon>
-		</view>
-		<view class="li_items d_flex" @tap="$tab.navigateTo('/pages/mine/settings/index')">
-			<view>{{$t('mine.text11')}}</view>
-			<u-icon name="arrow-right" color="#888" size="30rpx"></u-icon>
-		</view>
-		<view class="li_items d_flex" @tap="show=true">
-			<view>{{$t('mine.text12')}}</view>
-			<u-icon name="arrow-right" color="#888" size="30rpx"></u-icon>
-		</view>
-	</view>
-
-	<u-popup :show="show" @close="show=false" mode="center" round="16rpx">
-		<view class="quit_pop d_bgColor d_border12">
-			<view>
-				<view style="height: 40rpx"></view>
-				<view>{{$t('popUp.text4')}}</view>
-			</view>
-			<view class="d_flex">
-				<u-button type="primary" :text="$t('popUp.text2')" shape="circle" color="#22306b"
-					@tap="show=false"></u-button>
-				<u-button type="primary" :text="$t('popUp.text3')" shape="circle" @tap="logOutBtn"
-					color="linear-gradient(to right, rgb(34, 48, 107), rgb(0, 162, 251))"></u-button>
-			</view>
-		</view>
-	</u-popup>
-
-
-	<u-popup :show="showConversion" @close="showConversion=false" mode="center" round="10">
-		<view class="conversion_sty d_border12 d_bgColor">
-			<view class="one d_flex">
-				<view>
-					{{$t('mine.text13')}}:
+		<view class="d_m_vip d_bgColor border_shadow d_border12">
+			<view class="vi_top d_flex">
+				<view class="v_t_fl">
+					<u--image :showLoading="true" src="../../static/images/vip-background.png" width="200rpx"
+						height="160rpx" mode="scaleToFill">
+					</u--image>
+					<text>VIP {{userInfo.level}}</text>
 				</view>
-				
-				<!-- <scroll-view scroll-x class="o_d_scroll_view"> -->
-					<u-radio-group v-model="walletTransferForm.status" placement="column">
-						<u-radio :label="$t('mine.text20')" name="1" labelColor="#fff">
-						</u-radio>
-						<view style="height: 14rpx"></view>
-						<u-radio :label="$t('mine.text21')" name="2" labelColor="#fff">
-						</u-radio>
-						<view style="height: 14rpx"></view>
-						<u-radio :label="$t('mine.text22')" name="3" labelColor="#fff">
-						</u-radio>
-					</u-radio-group>
-				<!-- </scroll-view> -->
-			
-			</view>
-			<view class="tow"
-			v-show="walletTransferForm.status != 3"
-			>
-				<view>
-					{{walletTransferForm.status == '1'? $t('mine.text20'): $t('mine.text21')}}
+				<view class="v_t_fr">
+					<view style="margin-bottom: 10rpx;">
+						<text style="margin-right: 20rpx">{{$t('mine.text3')}}: </text>
+						<text style="color: #ff5a2c;">{{userInfo.recharge}}</text>
+					</view>
+					<view>
+						<u-line-progress :percentage="userInfo.recharge_b" activeColor="#37dfb1"></u-line-progress>
+					</view>
+					<view style="height: 24rpx;"></view>
+					<view style="margin-bottom: 10rpx;">
+						<text style="margin-right: 20rpx">{{$t('mine.text4')}}: </text>
+						<text style="color: #ff5a2c;">{{userInfo.bet}}</text>
+					</view>
+					<view>
+						<u-line-progress :percentage="userInfo.bet_b" activeColor="#37dfb1"></u-line-progress>
+					</view>
 				</view>
-				<u--input :placeholder="$t('mine.text19')" border="none" clearable :customStyle="{
-							background: '#2b2e3d',
-							padding: '20rpx'
-						}" v-model="walletTransferForm.money"
-						></u--input>
 			</view>
-			<view class="three">
-				{{$t('mine.text23')}}: {{userInfo.money}}
-			</view>
-			<view class="four">
-				<u-button type="primary" :text="$t('popUp.text1')" shape="circle" :customStyle="{height: '70rpx'}"
-					color="linear-gradient(to right, rgb(34, 48, 107), rgb(0, 162, 251))"
-					@tap="getWalletTransfer"></u-button>
+			<view class="vi_grid">
+				<u-grid :border="false" class="grid" col="2">
+					<u-grid-item class="vi_grid_item d_border12 d_flex"
+						@tap="$tab.navigateTo('/pages/mine/account/index?index=0')">
+						<text style="margin-right: 20rpx;" class="grid-text">{{$t('mine.text5')}}</text>
+						<u-icon name="../../static/images/conta.png" size="38rpx"></u-icon>
+					</u-grid-item>
+					<u-grid-item class="vi_grid_item d_border12 d_flex"
+						@tap="$tab.navigateTo('/pages/mine/account/index?index=1')">
+						<text style="margin-right: 20rpx;" class="grid-text">{{$t('mine.text6')}}</text>
+						<u-icon name="../../static/images/conta.png" size="38rpx"></u-icon>
+					</u-grid-item>
+				</u-grid>
 			</view>
 		</view>
-	</u-popup>
+		<view class="d_m_list border_shadow d_border12 d_bgColor">
+			<view style="margin-top: 20rpx; color: #46b5fb;">
+				{{$t('mine.text7')}}
+			</view>
+			<view class="li_items d_flex" @tap="$tab.navigateTo('/pages/mine/account/index?index=2')">
+				<view>{{$t('mine.text8')}}</view>
+				<u-icon name="arrow-right" color="#888" size="30rpx"></u-icon>
+			</view>
+
+			<view class="li_items d_flex" @tap="$tab.navigateTo('/pages/mine/backwater/index?index=2')">
+				<view>{{$t('title.text26')}}</view>
+				<u-icon name="arrow-right" color="#888" size="30rpx"></u-icon>
+			</view>
+
+
+			<view class="li_items d_flex" @tap="showConversion=true" v-if="false">
+				<view>{{$t('mine.text9')}}</view>
+				<u-icon name="arrow-right" color="#888" size="30rpx"></u-icon>
+			</view>
+			<view class="li_items d_flex" @tap="$tab.navigateTo('/pages/mine/play-record/index')">
+				<view>{{$t('mine.text10')}}</view>
+				<u-icon name="arrow-right" color="#888" size="30rpx"></u-icon>
+			</view>
+			<view class="li_items d_flex" @tap="$tab.navigateTo('/pages/mine/settings/index')">
+				<view>{{$t('mine.text11')}}</view>
+				<u-icon name="arrow-right" color="#888" size="30rpx"></u-icon>
+			</view>
+			<view class="li_items d_flex" @tap="show=true">
+				<view>{{$t('mine.text12')}}</view>
+				<u-icon name="arrow-right" color="#888" size="30rpx"></u-icon>
+			</view>
+		</view>
+
+		<u-popup :show="show" @close="show=false" mode="center" round="16rpx">
+			<view class="quit_pop d_bgColor d_border12">
+				<view>
+					<view style="height: 40rpx"></view>
+					<view>{{$t('popUp.text4')}}</view>
+				</view>
+				<view class="d_flex">
+					<u-button type="primary" :text="$t('popUp.text2')" shape="circle" color="#22306b"
+						@tap="show=false"></u-button>
+					<u-button type="primary" :text="$t('popUp.text3')" shape="circle" @tap="logOutBtn"
+						color="linear-gradient(to right, rgb(34, 48, 107), rgb(0, 162, 251))"></u-button>
+				</view>
+			</view>
+		</u-popup>
+
+
+		<u-popup :show="showConversion" @close="showConversion=false" mode="center" round="10">
+			<view class="conversion_sty d_border12 d_bgColor">
+				<view class="one d_flex">
+					<view>
+						{{$t('mine.text13')}}:
+					</view>
+
+					<!-- <scroll-view scroll-x class="o_d_scroll_view"> -->
+						<u-radio-group v-model="walletTransferForm.status" placement="column">
+							<u-radio :label="$t('mine.text20')" name="1" labelColor="#fff">
+							</u-radio>
+							<view style="height: 14rpx"></view>
+							<u-radio :label="$t('mine.text21')" name="2" labelColor="#fff">
+							</u-radio>
+							<view style="height: 14rpx"></view>
+							<u-radio :label="$t('mine.text22')" name="3" labelColor="#fff">
+							</u-radio>
+						</u-radio-group>
+					<!-- </scroll-view> -->
+
+				</view>
+				<view class="tow"
+				v-show="walletTransferForm.status != 3"
+				>
+					<view>
+						{{walletTransferForm.status == '1'? $t('mine.text20'): $t('mine.text21')}}
+					</view>
+					<u--input :placeholder="$t('mine.text19')" border="none" clearable :customStyle="{
+								background: '#2b2e3d',
+								padding: '20rpx'
+							}" v-model="walletTransferForm.money"
+							></u--input>
+				</view>
+				<view class="three">
+					{{$t('mine.text23')}}: {{userInfo.money}}
+				</view>
+				<view class="four">
+					<u-button type="primary" :text="$t('popUp.text1')" shape="circle" :customStyle="{height: '70rpx'}"
+						color="linear-gradient(to right, rgb(34, 48, 107), rgb(0, 162, 251))"
+						@tap="getWalletTransfer"></u-button>
+				</view>
+			</view>
+		</u-popup>
 	</view>
 </template>
 
